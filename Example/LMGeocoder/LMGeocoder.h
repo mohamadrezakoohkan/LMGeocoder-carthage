7d5f6a551965c49035844a305ad9054f2a8debb1
mohamad reza koohkan <mohamad_koohkan@icloud.com>
2022-12-13 11:22:48 +0300

    Add new shared scheme to build for Carthage

---
+++ Example/LMGeocoder/LMGeocoder.h
@@ -0,0 +1,19 @@
+//
+//  LMGeocoder.h
+//  LMGeocoder
+//
+//  Created by Mohammad reza on 13.12.2022.
+//  Copyright © 2022 LMinh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LMGeocoder.
+FOUNDATION_EXPORT double LMGeocoderVersionNumber;
+
+//! Project version string for LMGeocoder.
+FOUNDATION_EXPORT const unsigned char LMGeocoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LMGeocoder/PublicHeader.h>
+
+
